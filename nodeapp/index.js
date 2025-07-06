5ad125efb6e022994f0ccffccbcff4d72056f954
ShashithaU <udeshshashitha@gmail.com>
2025-07-06 23:35:35 +0530

    test_3

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -9,6 +9,7 @@ app.get("/", function (req, res) {
 app.get("/will", function (req, res) {
   res.send('{ "response": "Hello World" }');
 });
+//get res
 app.get("/ready", function (req, res) {
   res.send('{ "response": " Great!, It works!" }');
 });
